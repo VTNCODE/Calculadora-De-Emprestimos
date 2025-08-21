3eea3244588aeb2d79596922ed09b9b306ab441b
Vitor Gulak <vitorayrtongulakdeandrade@gmail.com>
2025-08-21 15:15:07 -0300

    Nova classe que será responsável por processar os valores do Data.

---
+++ src/services/ProcessData.java
@@ -0,0 +1,18 @@
+package services;
+
+import entities.Data;
+
+public class ProcessData {
+    private BankService service;
+
+    public ProcessData(BankService service) {
+        this.service = service;
+    }
+    public double processContract(Data values) {
+
+        return 1;
+
+
+    }
+
+}
