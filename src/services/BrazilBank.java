870a68baf9be6c92b1c29fa67795810b6066b136
Vitor Gulak <vitorayrtongulakdeandrade@gmail.com>
2025-08-22 15:05:48 -0300

    Mudança importante nesta classe, agora ela não é mais responsável por transformar as taxas em decimais. Função transferida para classe ProcessData.

--- src/services/BrazilBank.java
+++ src/services/BrazilBank.java
@@ -10,12 +10,12 @@ public class BrazilBank extends InterestRate implements BankService{
 
     @Override
     public double interestRate(double amount, double interestRate, int timeInMonths) {
-        return amount * Math.pow(1 + (interestRate/100), timeInMonths);
+        return amount * Math.pow(1 + interestRate, timeInMonths);
     }
 
     @Override
     public double contributionInterestRate(double amount, double interestRate, int timeInMonths, double contribution) {
-        return amount * Math.pow(1 + (interestRate/100), timeInMonths) + contribution * ((Math.pow(1 + (interestRate/100), timeInMonths) - 1) / (interestRate/100));
+        return amount * Math.pow(1 + interestRate, timeInMonths) + contribution * ((Math.pow(1 + interestRate, timeInMonths) - 1) / interestRate);
     }
 
 
