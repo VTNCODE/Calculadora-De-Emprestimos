7a8bae8cf3ea2d60b1f7a9fbf34151784ecf1ac6
Vitor Gulak <vitorayrtongulakdeandrade@gmail.com>
2025-08-22 15:04:29 -0300

    Finalizando o processo de converter o período e as taxas anuais para mensais.

--- src/services/ProcessData.java
+++ src/services/ProcessData.java
@@ -2,14 +2,57 @@ package services;
 
 import entities.Data;
 
+
+
 public class ProcessData {
-    private BankService service;
+    private final BankService service;
 
     public ProcessData(BankService service) {
         this.service = service;
     }
-    public double processContract(Data values) {
-        return 1;
+    public double processContractWithoutContribution(Data values) {
+        if (values.getTimePeriod() == Periodo.ANUAL) {
+            if (values.getInterestPeriod() == Periodo.ANUAL) {
+                double interest = (Math.pow((1 + values.getInterest()/100), ((double) 1 / 12)) - 1);
+                return service.interestRate(values.getAmount(), interest, values.getTime() * 12);
+            }
+            else  {
+                return service.interestRate(values.getAmount(), values.getInterest()/100, values.getTime() * 12);
+            }
+
+        }
+        else {
+            if (values.getInterestPeriod() == Periodo.ANUAL){
+                double interest = (Math.pow((1 + values.getInterest()/100), ((double) 1 / 12)) - 1);
+                return service.interestRate(values.getAmount(), interest, values.getTime());
+            }
+            else {
+                return service.interestRate(values.getAmount(), values.getInterest() / 100, values.getTime());
+            }
+        }
+
     }
 
+
+    public double processContractWithContribution(Data values) {
+        if (values.getTimePeriod() == Periodo.ANUAL) {
+            if (values.getInterestPeriod() == Periodo.ANUAL) {
+                double interest = (Math.pow((1 + (values.getInterest()/100)), ((double) 1 / 12)) - 1);
+                return service.contributionInterestRate(values.getAmount(), interest, values.getTime() * 12, values.getContribution());
+            }
+            else  {
+                return service.contributionInterestRate(values.getAmount(), values.getInterest()/100, values.getTime() * 12, values.getContribution());
+            }
+
+        }
+        else {
+            if (values.getInterestPeriod() == Periodo.ANUAL) {
+                double interest = (Math.pow((1 + values.getInterest()/100), ((double) 1 / 12)) - 1);
+                return service.contributionInterestRate(values.getAmount(), interest, values.getTime(), values.getContribution());
+            }
+            else {
+                return service.contributionInterestRate(values.getAmount(), values.getInterest()/100, values.getTime(), values.getContribution());
+            }
+        }
+    }
 }
