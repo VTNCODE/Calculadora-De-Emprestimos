847707fd8cdec6eadcb6718dff9efc6288119f6a
Vitor Gulak <vitorayrtongulakdeandrade@gmail.com>
2025-08-25 11:05:41 -0300

    Melhorando o código já que agora a contribuição mensal já é considerada inclusa, sem perguntas.

--- src/application/Program.java
+++ src/application/Program.java
@@ -24,41 +24,20 @@ public class Program {
         interestPeriod = sc.next().toUpperCase();
         System.out.print("Informe a taxa de juros: ");
         double interestRate = sc.nextDouble();
-        System.out.print("Deseja colocar um aporte mensal? (s/n): ");
-        char ansawer = sc.next().charAt(0);
-        if (ansawer == 's' || ansawer =='S') {
-            System.out.print("informe o aporte mensal: ");
-            double monthlyContribution = sc.nextDouble();
-            System.out.print("O período será mensal ou anual? (MENSAL/ANUAL) ");
-            contributionPeriod = sc.next().toUpperCase();
-            System.out.print("Informe a quantidade de tempo: ");
-            int time = sc.nextInt();
-            Data data = new Data(amount, Periodo.valueOf(interestPeriod), interestRate, monthlyContribution, Periodo.valueOf(contributionPeriod), time);
-            ProcessData processData = new ProcessData(new BrazilBank());
-            double total = processData.processContractWithContribution(data);
-            double totalInvest = processData.totalInvest(amount, monthlyContribution, time,Periodo.valueOf(contributionPeriod));
-            double totalInterest = processData.totalInterest(total, totalInvest);
-            System.out.println("Total investido: " + String.format("%.2f ",  + totalInvest));
-            System.out.println("Total ganho de juros: " + String.format("%.2f ", totalInterest));
-            System.out.println("Total: " + String.format("%.2f", processData.processContractWithContribution(data)));
-
-        }
-        else {
-            System.out.print("O período será mensal ou anual? (MENSAL/ANUAL) ");
-            contributionPeriod = sc.next().toUpperCase();
-            System.out.print("Informe a quantidade de tempo: ");
-            int time = sc.nextInt();
-            Data data = new Data(amount, Periodo.valueOf(interestPeriod), interestRate, Periodo.valueOf(contributionPeriod), time);
-            ProcessData processData = new ProcessData(new BrazilBank());
-            double total = processData.processContractWithoutContribution(data);
-            double totalInterest = processData.totalInterest(total, amount);
-            System.out.println("Total investido: " + String.format("%.2f ",  + amount));
-            System.out.println("Total ganho de juros: " + String.format("%.2f ", totalInterest));
-            System.out.println("Total: " + String.format("%.2f", processData.processContractWithoutContribution(data)));
-
-        }
-
-
+        System.out.print("informe o aporte mensal: ");
+        double monthlyContribution = sc.nextDouble();
+        System.out.print("O período será mensal ou anual? (MENSAL/ANUAL) ");
+        contributionPeriod = sc.next().toUpperCase();
+        System.out.print("Informe a quantidade de tempo: ");
+        int time = sc.nextInt();
+        Data data = new Data(amount, Periodo.valueOf(interestPeriod), interestRate, monthlyContribution, Periodo.valueOf(contributionPeriod), time);
+        ProcessData processData = new ProcessData(new BrazilBank());
+        double total = processData.processContract(data);
+        double totalInvest = processData.totalInvest(amount, monthlyContribution, time,Periodo.valueOf(contributionPeriod));
+        double totalInterest = processData.totalInterest(total, totalInvest);
+        System.out.println("Total investido: " + String.format("%.2f ",  + totalInvest));
+        System.out.println("Total ganho de juros: " + String.format("%.2f ", totalInterest));
+        System.out.println("Total: " + String.format("%.2f", processData.processContract(data)));
 
         sc.close();
     }
